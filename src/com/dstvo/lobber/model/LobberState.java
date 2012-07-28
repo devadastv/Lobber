73086b5e904b1cf27dd2f241915d6a52caef5399
Devadas.SunBird <devadastv@gmail.com>
2012-07-29 01:05:49 +0200

    [PARTIAL] LobberStatus.java renamed to LobberState

--- src/com/dstvo/lobber/model/LobberState.java
+++ src/com/dstvo/lobber/model/LobberState.java
@@ -8,10 +8,12 @@ package com.dstvo.lobber.model;
  *
  * @author user
  */
-public interface LobberStatus
+public interface LobberState
 {
     int PLAYER_WON = 1;
     int OPPONENT_WON = 2;
     int PLAYER_THINKING = 3;
     int WAITING_FOR_OPPONENT = 4;
+    int GAME_DRAWN = 5;
+    int WELCOME_PLAYER = 6;
 }
